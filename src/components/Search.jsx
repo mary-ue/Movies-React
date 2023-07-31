290146001123980c758e18cb712d0318e8a09031
Mary <maryandreeva100@gmail.com>
2023-07-31 17:47:28 +0300

    changed the Main component from classes to functional

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -17,72 +17,72 @@ const Search = (props) => {
     searchMovies(search, evt.target.dataset.type);
   };
 
-    return (
-      <>
-        <div className="row search-input-row">
-          <div className="input-field">
+  return (
+    <>
+      <div className="row search-input-row">
+        <div className="input-field">
+          <input
+            placeholder="search"
+            type="search"
+            className="validate validate-search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handleKey}
+          />
+          <button
+            className="btn search-btn"
+            onClick={() => searchMovies(search, type)}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="search-radio-btns">
+        <p>
+          <label>
             <input
-              placeholder="search"
-              type="search"
-              className="validate validate-search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyDown={handleKey}
+              className="with-gap"
+              name="type"
+              type="radio"
+              value=""
+              data-type="all"
+              onChange={handleFilter}
+              checked={type === 'all'}
             />
-            <button
-              className="btn search-btn"
-              onClick={() => searchMovies(search, type)}
-            >
-              Search
-            </button>
-          </div>
-        </div>
-        <div className="search-radio-btns">
-          <p>
-            <label>
-              <input
-                className="with-gap"
-                name="type"
-                type="radio"
-                value=""
-                data-type="all"
-                onChange={handleFilter}
-                checked={type === 'all'}
-              />
-              <span>All</span>
-            </label>
-          </p>
-          <p>
-            <label>
-              <input
-                className="with-gap"
-                name="type"
-                type="radio"
-                value="movie"
-                data-type="movie"
-                onChange={handleFilter}
-                checked={type === 'movie'}
-              />
-              <span>Movie only</span>
-            </label>
-          </p>
-          <p>
-            <label>
-              <input
-                className="with-gap"
-                name="type"
-                type="radio"
-                value="series"
-                data-type="series"
-                onChange={handleFilter}
-                checked={type === 'series'}
-              />
-              <span>Series only</span>
-            </label>
-          </p>
-        </div>
-      </>
-    );
-}
+            <span>All</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              className="with-gap"
+              name="type"
+              type="radio"
+              value="movie"
+              data-type="movie"
+              onChange={handleFilter}
+              checked={type === 'movie'}
+            />
+            <span>Movie only</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              className="with-gap"
+              name="type"
+              type="radio"
+              value="series"
+              data-type="series"
+              onChange={handleFilter}
+              checked={type === 'series'}
+            />
+            <span>Series only</span>
+          </label>
+        </p>
+      </div>
+    </>
+  );
+};
 
 export { Search };
